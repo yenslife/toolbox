46d3a8b20dc74bf27ebeffdc18da6065d749d57b
yenslife <77geo5rge6@gmail.com>
2024-11-17 03:16:29 +0800

    Add CORS middleware to allow frontend request

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers.file_transfer import router as file_transfer_router
 
@@ -6,6 +7,14 @@ app = FastAPI(
     title="線上工具箱，提供各種工具函數",
     description="提供各種工具函數，方便開發者使用",
     version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(file_transfer_router)
